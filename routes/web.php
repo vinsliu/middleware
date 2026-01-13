b9053c488e9ecd5951acb9d41c582e4f9b5a65a5
Vincent <vincent00lav@outlook.com>
2026-01-13 16:48:25 +0100

    Activité 5

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,6 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::resource('products', ProductController::class);
+Route::resource('products', ProductController::class)->middleware('auth');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
